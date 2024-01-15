d9638dd9f88f3095f9e15b687061d30f23745755
ansam007 <anshul.sharma5271@gmail.com>
2024-01-15 12:30:19 +0530

    half done structure stored user data in local storage

---
+++ src/Form/FormPage.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+
+const FormPage = (props) => {
+    const [currentID, setCurrentID] = useState('');
+    const [sellingCost, setsellingCost] = useState('');
+    const [name, setName] = useState('');
+
+    const ProductIDHandler = (event) => {
+        setCurrentID(event.target.value);
+    }
+
+    const SellingPriceHandler = (event) => {
+        setsellingCost(event.target.value);
+    }
+
+    const ProductNameHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const SubmitHandler = () => {
+        var data = {
+            ProductID : currentID,
+            SellingPrice : sellingCost,
+            ProductName : name,
+        }
+
+        const jsonString = JSON.stringify(data);
+        localStorage.setItem(data.ProductID, jsonString);
+        props.addItems(data);
+    }
+
+    return (
+        <form onSubmit={SubmitHandler}>
+            <label for="ProductID" style={{marginRight: "0.25rem"}}>ProductID:</label>
+            <input type="number" id="ProductID" style={{marginRight:"1rem"}} value={currentID} onChange={ProductIDHandler}></input>
+            <label for="SellingPrice" style={{marginRight: "0.25rem"}}>SellingPrice:</label>
+            <input type="text" id="SellingPrice" style={{marginRight:"1rem"}} value={sellingCost} onChange={SellingPriceHandler}></input>
+            <label for="ProductName" style={{marginRight: "0.25rem"}}>ProductName:</label>
+            <input type="text" id="ProductName" style={{marginRight:"0.50rem"}} value={name} onChange={ProductNameHandler}></input>
+            <button type="submit">Add Product</button>
+        </form>
+    )
+}
+export default FormPage;
